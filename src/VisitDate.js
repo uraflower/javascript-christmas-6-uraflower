20078f8fb413c8801ad93cfccded39d3de3501d8
uraflower <82873315+uraflower@users.noreply.github.com>
2023-11-10 16:12:35 +0900

    feat(날짜): 입력 받은 날짜가 온점 포함 시 예외 처리

--- src/VisitDate.js
+++ src/VisitDate.js
@@ -2,7 +2,22 @@ import EVENT_DATE from './constants/date/eventDate.js';
 
 class VisitDate extends Date {
   constructor(date) {
-    super(EVENT_DATE.year, EVENT_DATE.month, date);
+    super(EVENT_DATE.year, EVENT_DATE.month);
+    this.#validateDate(date);
+    this.setDate(date);
+  }
+
+  #validateDate(date) {
+    if (this.isIncludeDot(date)) {
+      throw new Error('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
+    }
+  }
+
+  isIncludeDot(date) {
+    if (date.constructor === String) {
+      return date.includes('.');
+    }
+    return false;
   }
 }
 
