4dff60d831b6e3a0b311289d86bfb712cb063348
uraflower <82873315+uraflower@users.noreply.github.com>
2023-11-10 16:37:35 +0900

    feat(날짜): 입력 값이 공백 포함하는 경우 예외 처리

--- src/VisitDate.js
+++ src/VisitDate.js
@@ -8,7 +8,7 @@ class VisitDate extends Date {
   }
 
   #validateDate(date) {
-    if (date === ' ' || date === '') {
+    if (date.includes(' ') || date === '') {
       throw new Error('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
     }
 
