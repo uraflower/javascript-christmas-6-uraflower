dc290a2220f3bd67c91e5c9e8251423311838da5
uraflower <82873315+uraflower@users.noreply.github.com>
2023-11-10 15:15:23 +0900

    feat(출력): 크리스마스 디데이 할인 혜택 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Discount from './Discount.js';
 import InputView from './View/InputView.js';
+import OutputView from './View/OutputView.js';
 
 class App {
   async run() {
     const day = await InputView.readDate();
     const discount = new Discount();
     const christmasDiscountedAmount = discount.discountChristmas(day);
+    OutputView.printBenefits(christmasDiscountedAmount);
   }
 }
 
--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -1,5 +1,20 @@
-export default OutputView = {
+import { Console } from '@woowacourse/mission-utils';
+
+const OutputView = {
   printMenu() {
     Console.print('<주문 메뉴>');
   },
+
+  printBenefits(christmasDiscountedAmount) {
+    Console.print('\n<혜택 내역>');
+    this.printChristmasDiscount(christmasDiscountedAmount);
+  },
+
+  printChristmasDiscount(christmasDiscountedAmount) {
+    if (christmasDiscountedAmount) {
+      Console.print(`크리스마스 디데이 할인: -${christmasDiscountedAmount}원`);
+    }
+  },
 };
+
+export default OutputView;
