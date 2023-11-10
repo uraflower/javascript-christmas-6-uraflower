4913795e806eb4f4a5c8d3b4f4e58dcbbd3b141b
uraflower <82873315+uraflower@users.noreply.github.com>
2023-11-10 16:41:47 +0900

    feat(날짜): 입력 값이 1~31 사이 숫자가 아닌 경우 예외 처리

--- src/VisitDate.js
+++ src/VisitDate.js
@@ -19,6 +19,10 @@ class VisitDate extends Date {
     if (!Number.isInteger(Number(date))) {
       throw new Error('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
     }
+
+    if (!(Number(date) >= 1 && Number(date) <= 31)) {
+      throw new Error('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
+    }
   }
 
   isIncludeDot(date) {
