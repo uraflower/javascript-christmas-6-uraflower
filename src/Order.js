291b304aac5a22c11bb2dc6ea76a9f2324955579
uraflower <82873315+uraflower@users.noreply.github.com>
2023-11-11 00:06:09 +0900

    feat(주문): 중복 메뉴 예외 처리

--- src/Order.js
+++ src/Order.js
@@ -1,4 +1,6 @@
 import COMMON from './constants/common.js';
+import ERROR from './constants/message/error.js';
+import CustomError from './errors/CustomError.js';
 import { parse } from './utils/format.js';
 
 class Order {
@@ -14,11 +16,19 @@ class Order {
 
     eachOrder.forEach((menuWithNumber) => {
       const [menu, number] = parse(menuWithNumber, COMMON.dash);
+      this.#validateDuplication(result, menu);
       result[menu] = number;
     });
 
     return result;
   }
+
+  #validateDuplication(order, menu) {
+    if (Object.hasOwn(order, menu)) {
+      throw new CustomError(ERROR.invalidOrder);
+    }
+  }
+
 }
 
 export default Order;
--- src/constants/message/error.js
+++ src/constants/message/error.js
@@ -2,6 +2,7 @@ const ERROR = Object.freeze({
   prefix: '[ERROR]',
   retry: '다시 입력해 주세요.',
   invalidDate: '유효하지 않은 날짜입니다.',
+  invalidOrder: '유효하지 않은 주문입니다.',
 });
 
 export default ERROR;
