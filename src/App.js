7d7266624d307860142b01c0f5691f775534059e
uraflower <82873315+uraflower@users.noreply.github.com>
2023-11-12 00:03:14 +0900

    feat(앱): 구현한 기능 및 모듈 불러와서 앱 구현
    - 방문 날짜 입력 받고 저장
    - 주문 입력 받고 저장
    - 주문 내역 출력
    - 할인 내역 계산
    - 할인 내역 출력

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import Discount from './Discount.js';
+import BenefitCalculator from './BenefitCalculator.js';
+import OrderManager from './OrderManager.js';
 import InputView from './View/InputView.js';
 import OutputView from './View/OutputView.js';
+import VisitDate from './VisitDate.js';
 
 class App {
+  #visitDate;
+
+  #orderManager;
+
   async run() {
-    const day = await InputView.readDate();
-    const discount = new Discount();
-    const christmasDiscountedAmount = discount.discountChristmas(day);
-    OutputView.printBenefits(christmasDiscountedAmount);
+    this.#visitDate = await this.#getVisitDate();
+    this.#orderManager = await this.#getOrderManager();
+
+    OutputView.printOrder(this.#orderManager.order);
+
+    const benefit = BenefitCalculator(this.#visitDate, this.#orderManager);
+    OutputView.printBenefits(benefit);
+  }
+
+  async #getVisitDate() {
+    const dateInput = await InputView.readDate();
+    const visitDate = new VisitDate(dateInput);
+    return visitDate;
+  }
+
+  async #getOrderManager() {
+    const orderInput = await InputView.readOrder();
+    const orderManager = new OrderManager(orderInput);
+    return orderManager;
   }
 }
 
