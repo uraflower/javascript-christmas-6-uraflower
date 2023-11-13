6de7b89da9d1550061fdf4729fbc14deb4fd32c6
uraflower <82873315+uraflower@users.noreply.github.com>
2023-11-13 18:53:50 +0900

    test(날짜): 방문 날짜 생성 테스트 추가

--- __tests__/VisitDateTest.js
+++ __tests__/VisitDateTest.js
@@ -2,6 +2,14 @@ import VisitDate from '../src/Domain/VisitDate';
 
 describe('방문 날짜 테스트', () => {
   describe('기능 테스트', () => {
+    test('유효한 날짜를 입력하면 방문 날짜를 생성한다', () => {
+      const date = '1';
+
+      expect(() => {
+        new VisitDate(date);
+      }).not.toThrow();
+    });
+
     test('입력 값이 3이면 방문 날짜의 날은 3일이다', () => {
       const date = '3';
       const expectedDate = 3;
