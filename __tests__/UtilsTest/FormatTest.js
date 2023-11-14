e01175ebf8d21e27cf3d64173ac33cfe89e012a2
uraflower <82873315+uraflower@users.noreply.github.com>
2023-11-14 21:44:03 +0900

    test(유틸): 금액 포맷 테스트 추가

--- __tests__/UtilsTest/FormatTest.js
+++ __tests__/UtilsTest/FormatTest.js
@@ -1,4 +1,4 @@
-import { formatCount } from '../../src/utils/format';
+import { formatCount, formatMoney } from '../../src/utils/format';
 
 describe('개수 포맷 테스트', () => {
   test('1을 개수로 포맷팅하면 "1개"이다', () => {
@@ -6,6 +6,17 @@ describe('개수 포맷 테스트', () => {
     const expected = '1개';
 
     const result = formatCount(number);
+
+    expect(result).toBe(expected);
+  });
+});
+
+describe('금액 포맷 테스트', () => {
+  test('100000을 금액으로 포맷팅하면 "100,000원"이다', () => {
+    const amount = 100_000;
+    const expected = '100,000원';
+
+    const result = formatMoney(amount);
 
     expect(result).toBe(expected);
   });
