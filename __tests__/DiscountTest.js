29ece0e5a1573cbd0c081a907f599cce71435527
uraflower <82873315+uraflower@users.noreply.github.com>
2023-11-14 15:53:46 +0900

    test(할인): 총 할인 금액 계산 테스트

--- __tests__/DiscountTest.js
+++ __tests__/DiscountTest.js
@@ -3,7 +3,7 @@ import OrderManager from '../src/Domain/OrderManager';
 import VisitDate from '../src/Domain/VisitDate.js';
 import BENEFIT from '../src/constants/benefit/benefit';
 
-const NO_AVAILABLE_ORDER = '타파스-1';
+const UNAVAILABLE_ORDER = '타파스-1';
 const AVAILABLE_ORDER = '타파스-1,바비큐립-2,초코케이크-3,샴페인-4';
 
 describe('크리스마스 디데이 할인 테스트', () => {
@@ -72,6 +72,24 @@ describe('특별 할인 테스트', () => {
 
       const discount = new Discount(visitDate, orderManager);
       const result = discount.detail[BENEFIT.special];
+
+      expect(result).toBe(expected);
+    },
+  );
+});
+
+describe('총 할인 금액 계산 테스트', () => {
+  test.each([
+    ['25', UNAVAILABLE_ORDER, 0],
+    ['25', AVAILABLE_ORDER, 3400 + 2023 * 3 + 1000],
+  ])(
+    '방문 날짜가 %s일이고 "%s" 주문 시 총 할인 금액은 %d원이다',
+    (date, order, expected) => {
+      const visitDate = new VisitDate(date);
+      const orderManager = new OrderManager(order);
+
+      const discount = new Discount(visitDate, orderManager);
+      const result = discount.getTotalAmountOfDiscount();
 
       expect(result).toBe(expected);
     },
