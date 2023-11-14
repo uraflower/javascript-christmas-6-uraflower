1d4135d832b3460da8bd9849d7f753dbc29841cd
uraflower <82873315+uraflower@users.noreply.github.com>
2023-11-14 21:45:27 +0900

    test(유틸): 마이너스 금액 포맷 테스트 추가

--- __tests__/UtilsTest/FormatTest.js
+++ __tests__/UtilsTest/FormatTest.js
@@ -1,4 +1,8 @@
-import { formatCount, formatMoney } from '../../src/utils/format';
+import {
+  formatCount,
+  formatMinusMoney,
+  formatMoney,
+} from '../../src/utils/format';
 
 describe('개수 포맷 테스트', () => {
   test('1을 개수로 포맷팅하면 "1개"이다', () => {
@@ -17,6 +21,15 @@ describe('금액 포맷 테스트', () => {
     const expected = '100,000원';
 
     const result = formatMoney(amount);
+
+    expect(result).toBe(expected);
+  });
+
+  test('100000을 마이너스 금액으로 포맷팅하면 "-100,000원"이다', () => {
+    const amount = 100_000;
+    const expected = '-100,000원';
+
+    const result = formatMinusMoney(amount);
 
     expect(result).toBe(expected);
   });
