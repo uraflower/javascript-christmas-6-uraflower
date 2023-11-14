c7e454b79786697e4f1a436f5538a110b880f230
uraflower <82873315+uraflower@users.noreply.github.com>
2023-11-14 15:43:12 +0900

    test(할인): 평일 할인 테스트 추가

--- __tests__/DiscountTest.js
+++ __tests__/DiscountTest.js
@@ -23,3 +23,21 @@ describe('크리스마스 디데이 할인 테스트', () => {
     },
   );
 });
+
+describe('평일 할인 테스트', () => {
+  test.each([
+    ['1', AVAILABLE_ORDER, 0],
+    ['25', AVAILABLE_ORDER, 2023 * 3],
+  ])(
+    '방문 날짜가 %s이고 "%s" 주문 시 %d원 할인한다',
+    (date, order, expected) => {
+      const visitDate = new VisitDate(date);
+      const orderManager = new OrderManager(order);
+
+      const discount = new Discount(visitDate, orderManager);
+      const result = discount.detail[BENEFIT.weekday];
+
+      expect(result).toBe(expected);
+    },
+  );
+});
