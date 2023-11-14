dacf02d4dbd6cab9f7d0c0076df674122288c446
uraflower <82873315+uraflower@users.noreply.github.com>
2023-11-14 15:44:32 +0900

    test(할인): 부자연스러운 문장 수정

--- __tests__/DiscountTest.js
+++ __tests__/DiscountTest.js
@@ -11,7 +11,7 @@ describe('크리스마스 디데이 할인 테스트', () => {
     ['31', AVAILABLE_ORDER, 0],
     ['25', AVAILABLE_ORDER, 3400],
   ])(
-    '방문 날짜가 %s이고 "%s" 주문 시 %d원 할인한다',
+    '방문 날짜가 %s일이고 "%s" 주문 시 %d원 할인한다',
     (date, order, expected) => {
       const visitDate = new VisitDate(date);
       const orderManager = new OrderManager(order);
@@ -29,7 +29,7 @@ describe('평일 할인 테스트', () => {
     ['1', AVAILABLE_ORDER, 0],
     ['25', AVAILABLE_ORDER, 2023 * 3],
   ])(
-    '방문 날짜가 %s이고 "%s" 주문 시 %d원 할인한다',
+    '방문 날짜가 %s일이고 "%s" 주문 시 %d원 할인한다',
     (date, order, expected) => {
       const visitDate = new VisitDate(date);
       const orderManager = new OrderManager(order);
